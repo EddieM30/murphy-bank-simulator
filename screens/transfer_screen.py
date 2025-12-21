fbafcd94f19398c938252dedf539451b48765f90
EddieM30 <murphyedward80@gmail.com>
2025-12-21 14:40:27 -0500

    feat(transfer): enhance transfer functionality with improved account selection and user prompts

--- screens/transfer_screen.py
+++ screens/transfer_screen.py
@@ -10,55 +10,39 @@ class TransferScreen:
     def show(self):
         while True:
             clear_console()
-            current_session = self.router.get_session
+            current_session = self.router.get_session()
             print('=== TRANSFER BETWEEN ACCOUNTS ===\n')
-            accounts = AccountsManager.list_account(
-                current_session['account_nickname'])
+            accounts = AccountsManager.list_account(current_session['user_id'])
             for i, account in enumerate(accounts):
-                print(f'{i + 1}. {account}\n')
+                print(f'{i + 1}. {account['account_nickname']}\n')
 
-            from_account = input(
-                'Which account would you like to transfer from? (input "n" to cancel): '))
-            if from_account.lower() == 'n':
+            choice = input(
+                'Which account would you like to transfer from? (input "n" to cancel): ')
+            if choice.lower() == 'n':
                 return 'dashboard'
+            from_account = accounts[int(choice) - 1]
+            input(f'{from_account['account_nickname']}')
             while True:
                 clear_console()
                 print('=== TRANSFER BETWEEN ACCOUNTS ===')
                 for i, account in enumerate(accounts):
-                    print(f'{i + 1}. {account}')
-                to_account= input(f'\nTransferring from {from_account} into: ')
-                if to_account.lower() == 'n':
+                    print(f'{i + 1}. {account['account_nickname']}')
+                choice = input(
+                    f'\nTransferring from {from_account["account_nickname"]} into: ')
+                if choice.lower() == 'n':
                     return 'dashboard'
+                to_account = accounts[int(choice) - 1]
                 if from_account == to_account:
                     print('Can\'t transfer money to same account??')
                     input('Press enter to continue...')
                     continue
                 break
             while True:
-                amount= input(f'How much would you like to move from {from_account} into {to_account}?: ')
+                amount = input(
+                    f'How much would you like to move from {from_account["account_nickname"]} into {to_account['account_nickname']}?: ')
                 if amount.lower() == 'n':
                     return 'dahsboard'
-                deci_amount= InputManager.clean_money_amount(amount)
+                deci_amount = InputManager.clean_money_amount(amount)
                 if deci_amount is None:
+                    print(deci_amount)
                     continue
-                placeholder = AccountsManager.transfer()
-
-            else:
-                amount= input('How much would you like to transfer?\n$')
-                validate_amount= current_account.validate_money(amount)
-                if validate_amount is None:
-                    continue
-
-                else:
-
-                if current_account.transfer(
-              account_name, accounts.accounts, validate_amount):
-               utils.clear_console()
-               print(
-               f"Success!! ${validate_amount:,.2f} transferred to {account_name.title()}")
-                print(f"New balance: ${current_account.balance:,.2f}")
-                input('\nPress enter to continue...')
-                account_menu(current_account)
-                else:
-                input(
-                "Transfer failed - check name or amount. \nPress enter...")
